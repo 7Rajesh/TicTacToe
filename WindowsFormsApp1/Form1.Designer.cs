391f3ef26048f2ddd53324ef94fb634bd1dc93f0
Rajesh <7rajeshmeena@gmail.com>
2023-07-29 16:23:28 +0530

    bottun input and checker

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btn1 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn3 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
-            this.btn5 = new System.Windows.Forms.Button();
-            this.btn4 = new System.Windows.Forms.Button();
             this.btn9 = new System.Windows.Forms.Button();
             this.btn8 = new System.Windows.Forms.Button();
             this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
             this.resetbtn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -60,61 +60,6 @@
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // btn1
-            // 
-            this.btn1.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn1.Location = new System.Drawing.Point(18, 29);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(62, 61);
-            this.btn1.TabIndex = 1;
-            this.btn1.Text = "O";
-            this.btn1.UseVisualStyleBackColor = true;
-            // 
-            // btn2
-            // 
-            this.btn2.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn2.Location = new System.Drawing.Point(86, 29);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(62, 61);
-            this.btn2.TabIndex = 2;
-            this.btn2.UseVisualStyleBackColor = true;
-            // 
-            // btn3
-            // 
-            this.btn3.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn3.Location = new System.Drawing.Point(154, 29);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(62, 61);
-            this.btn3.TabIndex = 3;
-            this.btn3.UseVisualStyleBackColor = true;
-            // 
-            // btn6
-            // 
-            this.btn6.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn6.Location = new System.Drawing.Point(154, 96);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(62, 61);
-            this.btn6.TabIndex = 6;
-            this.btn6.UseVisualStyleBackColor = true;
-            // 
-            // btn5
-            // 
-            this.btn5.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn5.Location = new System.Drawing.Point(86, 96);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(62, 61);
-            this.btn5.TabIndex = 5;
-            this.btn5.UseVisualStyleBackColor = true;
-            // 
-            // btn4
-            // 
-            this.btn4.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn4.Location = new System.Drawing.Point(18, 96);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(62, 61);
-            this.btn4.TabIndex = 4;
-            this.btn4.UseVisualStyleBackColor = true;
-            // 
             // btn9
             // 
             this.btn9.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -123,6 +68,7 @@
             this.btn9.Size = new System.Drawing.Size(62, 61);
             this.btn9.TabIndex = 9;
             this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
             // 
             // btn8
             // 
@@ -132,6 +78,7 @@
             this.btn8.Size = new System.Drawing.Size(62, 61);
             this.btn8.TabIndex = 8;
             this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
             // 
             // btn7
             // 
@@ -141,6 +88,67 @@
             this.btn7.Size = new System.Drawing.Size(62, 61);
             this.btn7.TabIndex = 7;
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(154, 96);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(62, 61);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(86, 96);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(62, 61);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(18, 96);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(62, 61);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(154, 29);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(62, 61);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(86, 29);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(62, 61);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Ravie", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(18, 29);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(62, 61);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // resetbtn
             // 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,11 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        bool checker;
         public Form1()
         {
             InitializeComponent();
+             
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -28,11 +30,165 @@ namespace WindowsFormsApp1
             btn7.Text = "";
             btn8.Text = "";
             btn9.Text = "";
+
+            checker = false;
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            
+        }
 
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (btn1.Text == "")
+            {
+                if (checker==false) {
+                    btn1.Text = "O";
+                    checker=true;
+                }
+                else if (checker == true){
+                    btn1.Text = "X";
+                    checker=false;
+                }
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (btn2.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn2.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn2.Text = "X";
+                    checker = false;
+                }
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (btn3.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn3.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn3.Text = "X";
+                    checker = false;
+                }
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (btn4.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn4.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn4.Text = "X";
+                    checker = false;
+                }
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (btn5.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn5.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn5.Text = "X";
+                    checker = false;
+                }
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (btn6.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn6.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn6.Text = "X";
+                    checker = false;
+                }
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (btn7.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn7.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn7.Text = "X";
+                    checker = false;
+                }
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (btn8.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn8.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn8.Text = "X";
+                    checker = false;
+                }
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (btn9.Text == "")
+            {
+                if (checker == false)
+                {
+                    btn9.Text = "O";
+                    checker = true;
+                }
+                else if (checker == true)
+                {
+                    btn9.Text = "X";
+                    checker = false;
+                }
+            }
         }
     }
 }
+ 
